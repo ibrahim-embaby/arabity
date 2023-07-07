33919970390239e18e07b5be1c6129b3d696996d
ibrahim-embaby <ibrahimembaby10@gmail.com>
2023-07-07 06:56:41 +0300

    adding baseURL attribute to index.hs

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter baseURL="/#/">
     <Provider store={store}>
       <App />
     </Provider>
