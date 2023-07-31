0b855095afa3fd9645e073c18acb9bc2f5105de8
ibrahim-embaby <ibrahimembaby10@gmail.com>
2023-07-31 19:06:35 +0300

    change request url

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://localhost:8000", //"https://arabity.onrender.com",
+  baseURL: "https://arabity.onrender.com", // "http://localhost:8000",
 });
 
 export default request;
