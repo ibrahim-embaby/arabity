cfb76a1d211445a4f5c1cacad247a2558f7f7a1a
ibrahim-embaby <ibrahimembaby10@gmail.com>
2024-01-21 00:06:46 +0200

    made select components background white and added circular progress bar on home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -9,6 +9,7 @@ import MessageIcon from "@mui/icons-material/Message";
 import appVision from "../../assets/app-vision.jpg";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchControls } from "../../redux/apiCalls/controlsApiCalls";
+import CircularProgress from "@mui/joy/CircularProgress";
 // import Testimonial from "./Testimonial";
 
 // import user1 from "../../assets/user-1.jpg";
@@ -81,198 +82,206 @@ function Home() {
     });
   };
 
-  return (
-    !loading && (
-      <div className="home">
-        <section className="home-top">
-          <div className="home-top-wrapper">
-            <h1 className="home-top-title">{t("home_top_section_title")}</h1>
-            <form
-              className="search-bar"
-              onSubmit={searchFormHandler}
-              style={{ direction: i18n.language === "en" ? "ltr" : "rtl" }}
-            >
-              <div className="search-bar-inputs-wrapper">
-                <div
-                  className="search-bar-select-wrapper"
+  return loading ? (
+    <div
+      className="loading-page"
+      style={{
+        minHeight: "calc(100vh - var(--difference-value))",
+      }}
+    >
+      <CircularProgress color="primary" />
+    </div>
+  ) : (
+    <div className="home">
+      <section className="home-top">
+        <div className="home-top-wrapper">
+          <h1 className="home-top-title">{t("home_top_section_title")}</h1>
+          <form
+            className="search-bar"
+            onSubmit={searchFormHandler}
+            style={{ direction: i18n.language === "en" ? "ltr" : "rtl" }}
+          >
+            <div className="search-bar-inputs-wrapper">
+              <div
+                className="search-bar-select-wrapper"
+                style={{
+                  backgroundColor: service && "#ffe285",
+                  border: service && "black",
+                }}
+              >
+                <select
+                  className="search-bar-item"
+                  value={service ? JSON.stringify(service) : ""}
+                  onChange={(e) => setService(JSON.parse(e.target.value))}
                   style={{
                     backgroundColor: service && "#ffe285",
-                    border: service && "black",
+                    color: service && "black",
                   }}
                 >
-                  <select
-                    className="search-bar-item"
-                    value={service ? JSON.stringify(service) : ""}
-                    onChange={(e) => setService(JSON.parse(e.target.value))}
-                    style={{
-                      backgroundColor: service && "#ffe285",
-                      color: service && "black",
-                    }}
+                  <option
+                    className="search-bar-item-option"
+                    value={""}
+                    disabled
                   >
+                    {t("service_select")}
+                  </option>
+                  {services?.map((service) => (
                     <option
                       className="search-bar-item-option"
-                      value={""}
-                      disabled
+                      key={service.value}
+                      value={JSON.stringify(service)}
                     >
-                      {t("service_select")}
+                      {service.label[i18n.language]}
                     </option>
-                    {services?.map((service) => (
-                      <option
-                        className="search-bar-item-option"
-                        key={service.value}
-                        value={JSON.stringify(service)}
-                      >
-                        {service.label[i18n.language]}
-                      </option>
-                    ))}
-                  </select>
-                  {service && (
-                    <HighlightOffIcon
-                      sx={{
-                        color: "red",
-                        cursor: "pointer",
-                      }}
-                      onClick={() => setService("")}
-                    />
-                  )}
-                </div>
+                  ))}
+                </select>
+                {service && (
+                  <HighlightOffIcon
+                    sx={{
+                      color: "red",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => setService("")}
+                  />
+                )}
+              </div>
 
-                <div
-                  className="search-bar-select-wrapper"
+              <div
+                className="search-bar-select-wrapper"
+                style={{
+                  backgroundColor: car && "#ffe285",
+                  border: car && "none",
+                }}
+              >
+                <select
+                  className="search-bar-item"
+                  value={car ? JSON.stringify(car) : ""}
+                  onChange={(e) => setCar(JSON.parse(e.target.value))}
                   style={{
                     backgroundColor: car && "#ffe285",
-                    border: car && "none",
+                    color: car && "black",
                   }}
                 >
-                  <select
-                    className="search-bar-item"
-                    value={car ? JSON.stringify(car) : ""}
-                    onChange={(e) => setCar(JSON.parse(e.target.value))}
-                    style={{
-                      backgroundColor: car && "#ffe285",
-                      color: car && "black",
-                    }}
+                  <option
+                    className="search-bar-item-option"
+                    value={""}
+                    disabled
                   >
+                    {t("car_select")}
+                  </option>
+                  {cars.map((car) => (
                     <option
                       className="search-bar-item-option"
-                      value={""}
-                      disabled
+                      key={car.value}
+                      value={JSON.stringify(car)}
                     >
-                      {t("car_select")}
+                      {car.label[i18n.language]}
                     </option>
-                    {cars.map((car) => (
-                      <option
-                        className="search-bar-item-option"
-                        key={car.value}
-                        value={JSON.stringify(car)}
-                      >
-                        {car.label[i18n.language]}
-                      </option>
-                    ))}
-                  </select>
-                  {car && (
-                    <HighlightOffIcon
-                      sx={{
-                        color: "red",
-                        cursor: "pointer",
-                      }}
-                      onClick={() => setCar("")}
-                    />
-                  )}
-                </div>
+                  ))}
+                </select>
+                {car && (
+                  <HighlightOffIcon
+                    sx={{
+                      color: "red",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => setCar("")}
+                  />
+                )}
+              </div>
 
-                <div
-                  className="search-bar-select-wrapper"
+              <div
+                className="search-bar-select-wrapper"
+                style={{
+                  backgroundColor: province && "#ffe285",
+                  border: province && "none",
+                }}
+              >
+                <select
+                  className="search-bar-item"
+                  value={province ? JSON.stringify(province) : ""}
+                  onChange={(e) => setProvince(JSON.parse(e.target.value))}
                   style={{
                     backgroundColor: province && "#ffe285",
-                    border: province && "none",
+                    color: province && "black",
                   }}
                 >
-                  <select
-                    className="search-bar-item"
-                    value={province ? JSON.stringify(province) : ""}
-                    onChange={(e) => setProvince(JSON.parse(e.target.value))}
-                    style={{
-                      backgroundColor: province && "#ffe285",
-                      color: province && "black",
-                    }}
+                  <option
+                    className="search-bar-item-option"
+                    value={""}
+                    disabled
                   >
+                    {t("province_select")}
+                  </option>
+                  {provinces.map((province) => (
                     <option
                       className="search-bar-item-option"
-                      value={""}
-                      disabled
+                      key={province.value}
+                      value={JSON.stringify(province)}
                     >
-                      {t("province_select")}
+                      {province.label[i18n.language]}
                     </option>
-                    {provinces.map((province) => (
-                      <option
-                        className="search-bar-item-option"
-                        key={province.value}
-                        value={JSON.stringify(province)}
-                      >
-                        {province.label[i18n.language]}
-                      </option>
-                    ))}
-                  </select>
-                  {province && (
-                    <HighlightOffIcon
-                      sx={{
-                        color: "red",
-                        cursor: "pointer",
-                      }}
-                      onClick={() => setProvince("")}
-                    />
-                  )}
-                </div>
+                  ))}
+                </select>
+                {province && (
+                  <HighlightOffIcon
+                    sx={{
+                      color: "red",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => setProvince("")}
+                  />
+                )}
               </div>
-              <button className="search-bar-btn" type="submit">
-                {t("search_btn")}
-              </button>
-            </form>
-          </div>
-        </section>
-        <div
-          className="container"
-          style={{ direction: i18n.language === "en" ? "ltr" : "rtl" }}
-        >
-          <div className="home-sections-wrapper">
-            <section className="home-app-features">
-              <div className="home-app-features-item">
-                <PersonAddAltIcon className="home-app-features-item-icon" />
-                <p>{t("home_card_create_account")}</p>
-              </div>
+            </div>
+            <button className="search-bar-btn" type="submit">
+              {t("search_btn")}
+            </button>
+          </form>
+        </div>
+      </section>
+      <div
+        className="container"
+        style={{ direction: i18n.language === "en" ? "ltr" : "rtl" }}
+      >
+        <div className="home-sections-wrapper">
+          <section className="home-app-features">
+            <div className="home-app-features-item">
+              <PersonAddAltIcon className="home-app-features-item-icon" />
+              <p>{t("home_card_create_account")}</p>
+            </div>
 
-              <div className="home-app-features-item">
-                <SearchIcon className="home-app-features-item-icon" />
-                <p>{t("home_card_browse")}</p>
-              </div>
+            <div className="home-app-features-item">
+              <SearchIcon className="home-app-features-item-icon" />
+              <p>{t("home_card_browse")}</p>
+            </div>
 
-              <div className="home-app-features-item ">
-                <MessageIcon className="home-app-features-item-icon" />
-                <p>{t("home_card_contact")}</p>
-              </div>
-            </section>
+            <div className="home-app-features-item ">
+              <MessageIcon className="home-app-features-item-icon" />
+              <p>{t("home_card_contact")}</p>
+            </div>
+          </section>
 
-            <section className="home-about-app">
-              <div className="home-about-app-right">
-                <img
-                  className="home-about-app-right-img"
-                  src={appVision}
-                  alt=""
-                />
-              </div>
-              <div className="home-about-app-left">
-                <p>{t("our_vision_desc")}</p>
-                <p>{t("our_vision_desc")}</p>
-              </div>
-            </section>
+          <section className="home-about-app">
+            <div className="home-about-app-right">
+              <img
+                className="home-about-app-right-img"
+                src={appVision}
+                alt=""
+              />
+            </div>
+            <div className="home-about-app-left">
+              <p>{t("our_vision_desc")}</p>
+              <p>{t("our_vision_desc")}</p>
+            </div>
+          </section>
 
-            {/* <section className="testimonial">
+          {/* <section className="testimonial">
           <div className="testimonial-wrapper">
             <Testimonial />
           </div>
         </section> */}
-            {/* <section className="testimonial">
+          {/* <section className="testimonial">
           <div className="testimonial-wrapper">
             {testimonials.map((testimonial, index) => (
               <Testimonial
@@ -285,10 +294,9 @@ function Home() {
             ))}
           </div>
         </section> */}
-          </div>
         </div>
       </div>
-    )
+    </div>
   );
 }
 
