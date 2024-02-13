83020fbb3ce381dd8495b2c2600db916312921b8
ibrahim-embaby <ibrahimembaby10@gmail.com>
2024-02-13 11:36:15 +0200

    added validation if user is sigend and not verified his mail, he can't see the posts page

--- client/src/App.js
+++ client/src/App.js
@@ -58,7 +58,7 @@ function App() {
         />
         <Route path="/search/workshops" element={<SearchResults />} />
         <Route path="/mechanic/profile/:id" element={user && !user.isAccountVerified ? <VerifyAccount /> : <MechanicProfile />} />
-        <Route path="/posts" element={user && user.isAccountVerified ? <Posts /> : <Navigate to={"/"} />} />
+        <Route path="/posts" element={user && !user.isAccountVerified ? <Navigate to={"/"} /> : <Posts />} />
         <Route
           path="/conversations"
           element={user && user.isAccountVerified ? <Conversations /> : <Navigate to={"/"} />}
